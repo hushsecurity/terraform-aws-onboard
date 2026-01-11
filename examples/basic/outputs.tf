ac0aabc62a980723e72814cad658f79ae7ac081c
Alon Horowitz <alon@hush.security>
2026-01-11 17:55:44 +0200

    HUSH-2350 examples: basic example for module usage

---
+++ examples/basic/outputs.tf
@@ -0,0 +1,3 @@
+output "role_arn" {
+  value = module.hush_security.role_arn
+}
---
+++ examples/basic/variables.tf
@@ -0,0 +1,4 @@
+variable "hush_org_id" {
+  description = "Hush Security organization ID."
+  type        = string
+}
