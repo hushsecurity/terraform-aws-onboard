253fdefd86a94eae25b3155731a2765e40a32926
Alon Horowitz <alon@hush.security>
2026-01-21 14:34:49 +0200

    HUSH-2350 stackset: support multi account deployment using stackset

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,20 @@ output "role_name" {
   description = "Name of the IAM role"
   value       = aws_iam_role.this.name
 }
+
+# StackSet mode outputs
+output "stackset_arn" {
+  description = "ARN of the CloudFormation StackSet (stackset mode only)"
+  value       = var.type == "stackset" ? aws_cloudformation_stack_set.this[0].arn : null
+}
+
+output "stackset_name" {
+  description = "Name of the CloudFormation StackSet (stackset mode only)"
+  value       = var.type == "stackset" ? aws_cloudformation_stack_set.this[0].name : null
+}
+
+# Common output
+output "unique_suffix" {
+  description = "Unique suffix for role naming (needed for mixer registration in stackset mode)"
+  value       = random_id.suffix.hex
+}
